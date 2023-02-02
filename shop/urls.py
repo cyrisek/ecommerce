44ed9f8431369f987d4e61cfa6b752ea8bc841fc
Mateusz Urban <58361072+cyrisek@users.noreply.github.com>
2023-02-02 18:54:02 +0000

    adding and removing from the cart

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.item_list, name='item-list'),
+    path('', views.HomeView.as_view(), name='item-list'),
+    path('product/<slug>', views.ItemDetailView.as_view(), name='product'),
+    path('add_to_cart/<slug>', views.add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<slug>', views.remove_from_cart, name='remove_from_cart'),
 ]
