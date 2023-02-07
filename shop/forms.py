6a17abb7407a4f20fd2ed5640177318390192b1b
Mateusz Urban <58361072+cyrisek@users.noreply.github.com>
2023-02-07 18:10:32 +0000

    cheackout and billing

--- shop/forms.py
+++ shop/forms.py
@@ -1,12 +1,31 @@
 from django import forms
 from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
+
+
+PAYMENT_CHOICES = (
+    ('S', 'Stripe'),
+    ('P', 'Paypal')
+)
 
 
 class CheckoutForm(forms.Form):
-    street_address = forms.CharField()
-    apartment_address = forms.CharField(required=False)
-    country = CountryField(blank_label='select country')
-    postcode = forms.CharField()
-    same_billing_adress = forms.BooleanField(widget=forms.CheckboxInput())
-    save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.BooleanField(widget=forms.RadioSelect())
+    street_address = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '123 Main St'
+    }))
+    apartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'placeholder': 'Apartment of flat'
+    }))
+    country = CountryField(blank_label='select country').formfield(
+        widget=CountrySelectWidget(attrs={
+            'class': 'custom-select d-block w-100'
+        }))
+    postcode = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+    same_billing_adress = forms.BooleanField(
+        widget=forms.CheckboxInput(), required=False)
+    save_info = forms.BooleanField(
+        widget=forms.CheckboxInput(), required=False)
+    payment_option = forms.ChoiceField(
+        widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
